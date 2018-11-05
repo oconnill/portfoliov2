7143ecd7c4ed6223f4d35468de65b6f6ca28c24d
oconnill <doconnelldesign@gmail.com>
2018-11-05 14:36:50 -0700

    Removed default helloworld create box component

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
 <template>
   <div id="app">
-    <div class="container">
-        <div class="row">
-            <div class="col small red"></div>
-            <div class="col small blue"></div>
-            <div class="col small">Here</div>
-          </div>
-    </div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<ControlBox></ControlBox>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import '../public/style.scss'
+import ControlBox from './components/ControlBox.vue'
+
 import '../node_modules/bootstrap/scss/bootstrap.scss';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ControlBox
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ControlBox.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col small red"></div>
+            <div class="col small blue"></div>
+            <div class="col small">Here</div>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+    import '../../public/controlBox.scss'
+
+    export default {
+        name: 'app',
+    }
+</script>
