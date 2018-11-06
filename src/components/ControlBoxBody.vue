3ae61a9a1364e95f946750c79404c3bd7764d33c
oconnill <doconnelldesign@gmail.com>
2018-11-06 12:14:16 -0700

    add menu options

--- src/components/ControlBoxBody.vue
+++ src/components/ControlBoxBody.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <div class="control-box-body" v-for="typeOfWork in typeOfWorks">
-            <h3><a href="" @click="activeClick(typeOfWork.name)">{{ typeOfWork.name }}</a></h3>
+        <div class="control-box-body" v-for="standardMenuOption in standardMenuOptions">
+            <h3><a href="" @click="activeClick(standardMenuOption.name)">{{ standardMenuOption.name }}</a></h3>
         </div>
     </div>
 </template>
@@ -14,26 +14,92 @@
         name: 'ControlBoxBody',
         data() {
             return {
-                typeOfWorks: [
+                standardMenuOptions: [
                     { name: 'development' },
                     { name: 'design' },
                     { name: 'photography' }
                 ],
+                developmentOptions: [
+                    {
+                        name: 'RV Idaho',
+                        // image: require('../assets/rvidaho_hover.png'),
+                        description: 'Built with a development team, a form for brochure sign-up.  All entries could be exported by admin to a .csv file type. Laravel, Javascript, and WordPress were used in the development.',
+                        demoLink: 'https://rvidaho.org/free-camping-guide/',
+                    },
+                    {
+                        name: 'Brundage',
+                        // image: require('../assets/brundage_hover.png'),
+                        description: 'Updated homepage weather widget and worked to bring in a graph displaying snow data for base and summit depth',
+                        demoLink: 'https://brundage.com/',
+                    },
+                    {
+                        name: 'Kanban',
+                        // image: require('../assets/kanban-hover.png'),
+                        description: 'Organize work flow with custom boards, lists, tasks, and notes',
+                        demoLink: "https://kanban-oconnell.herokuapp.com/",
+                        gitHubLink: 'https://github.com/oconnill/vue-kanban'
+                    },
+                    {
+                        name: 'Music Selector',
+                        // image: require('../assets/music-selector-hover.png'),
+                        description: 'Build a playlist of songs from the I Tunes API',
+                        demoLink: 'https://vue-music-oconnell.herokuapp.com/',
+                        gitHubLink: 'https://github.com/oconnill/vue-music-checkpoint'
+                    },
+                    {
+                        name: 'Univ-Rss',
+                        // image: require('../assets/univrss-hover.png'),
+                        description: 'Personal dashboard for weather, rss feeds, local events, todo lists, photos, and spotify',
+                        demoLink: '',
+                        gitHubLink: 'https://github.com/oconnill/Univ-Rss'
+                    }
+                ],
+                designOptions: [
+                    {
+                        name: 'Yearly Poster Project 1986 - 1994',
+                        // image: require('../assets/univrss-hover.png'),
+                        description: 'Personal dashboard for weather, rss feeds, local events, todo lists, photos, and spotify',
+                        demoLink: '',
+                        gitHubLink: 'https://github.com/oconnill/Univ-Rss'
+                    }
+                ],
+                photographyOptions: [
+                    {
+                        name: 'Dublin Summer 1990',
+                        // image: require('../assets/univrss-hover.png'),
+                        description: 'Personal dashboard for weather, rss feeds, local events, todo lists, photos, and spotify',
+                        demoLink: '',
+                        gitHubLink: 'https://github.com/oconnill/Univ-Rss'
+                    }
+                ]
+                ,
                 isOpen: false,
                 menuChoice: ''
             }
         },
         methods: {
             activeClick(choice) {
-                console.log(name)
-                debugger
+
+                switch (choice) {
+                    case 'development':
+                    this.standardMenuOptions = this.developmentOptions
+                    debugger
+                        break
+                    case 'design':
+                    this.standardMenuOptions = this.designOptions
+                        break
+                    case 'photography':
+                    this.standardMenuOptions = this.photographyOptions
+                        break
+                }
+
             },
             open() {
-                
+
                 this.isOpen = true;
                 //if this create a switch statement then create the text to populate the direct menu
-        }
+            }
 
+        }
     }
-}
 </script>
