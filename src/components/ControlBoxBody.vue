0981b17edd623b6db51c001d98fca600d8cd35ab
oconnill <doconnelldesign@gmail.com>
2018-11-06 14:29:13 -0700

    can hit the store for changing state of menu options

--- src/components/ControlBoxBody.vue
+++ src/components/ControlBoxBody.vue
@@ -17,60 +17,6 @@
         data() {
             return {
                 standardMenuOptions: [],
-                developmentOptions: [
-                    {
-                        name: 'RV Idaho',
-                        // image: require('../assets/rvidaho_hover.png'),
-                        description: 'Built with a development team, a form for brochure sign-up.  All entries could be exported by admin to a .csv file type. Laravel, Javascript, and WordPress were used in the development.',
-                        demoLink: 'https://rvidaho.org/free-camping-guide/',
-                    },
-                    {
-                        name: 'Brundage',
-                        // image: require('../assets/brundage_hover.png'),
-                        description: 'Updated homepage weather widget and worked to bring in a graph displaying snow data for base and summit depth',
-                        demoLink: 'https://brundage.com/',
-                    },
-                    {
-                        name: 'Kanban',
-                        // image: require('../assets/kanban-hover.png'),
-                        description: 'Organize work flow with custom boards, lists, tasks, and notes',
-                        demoLink: "https://kanban-oconnell.herokuapp.com/",
-                        gitHubLink: 'https://github.com/oconnill/vue-kanban'
-                    },
-                    {
-                        name: 'Music Selector',
-                        // image: require('../assets/music-selector-hover.png'),
-                        description: 'Build a playlist of songs from the I Tunes API',
-                        demoLink: 'https://vue-music-oconnell.herokuapp.com/',
-                        gitHubLink: 'https://github.com/oconnill/vue-music-checkpoint'
-                    },
-                    {
-                        name: 'Univ-Rss',
-                        // image: require('../assets/univrss-hover.png'),
-                        description: 'Personal dashboard for weather, rss feeds, local events, todo lists, photos, and spotify',
-                        demoLink: '',
-                        gitHubLink: 'https://github.com/oconnill/Univ-Rss'
-                    }
-                ],
-                designOptions: [
-                    {
-                        name: 'Yearly Poster Project 1986 - 1994',
-                        // image: require('../assets/univrss-hover.png'),
-                        description: 'Personal dashboard for weather, rss feeds, local events, todo lists, photos, and spotify',
-                        demoLink: '',
-                        gitHubLink: 'https://github.com/oconnill/Univ-Rss'
-                    }
-                ],
-                photographyOptions: [
-                    {
-                        name: 'Dublin Summer 1990',
-                        // image: require('../assets/univrss-hover.png'),
-                        description: 'Personal dashboard for weather, rss feeds, local events, todo lists, photos, and spotify',
-                        demoLink: '',
-                        gitHubLink: 'https://github.com/oconnill/Univ-Rss'
-                    }
-                ]
-                ,
                 isOpen: false,
                 developmentTitle: ''
             }
@@ -79,19 +25,20 @@
             /// returns default menu options
             return this.standardMenuOptions = this.$store.state.standardMenuOptions
         },
+//         computed: {
+// menud
+//         },
         methods: {
             activeClick(choice) {
-
                 switch (choice) {
                     case 'development':
                         this.developmentTitle = choice
-                        this.standardMenuOptions = this.developmentOptions
+                        this.standardMenuOptions = this.$store.state.developmentOptions 
                         break
                     case 'design':
-                        this.standardMenuOptions = this.designOptions
-                        break
+                        this.standardMenuOptions = this.$store.state.designOptions 
                     case 'photography':
-                        this.standardMenuOptions = this.photographyOptions
+                        this.standardMenuOptions = this.$store.state.photographyOptions
                         break
                 }
 
--- store/store.js
+++ store/store.js
@@ -10,6 +10,59 @@ export const store = new vuex.Store({
             { name: 'design' },
             { name: 'photography' }
         ],
+        developmentOptions: [
+          {
+              name: 'RV Idaho',
+              // image: require('../assets/rvidaho_hover.png'),
+              description: 'Built with a development team, a form for brochure sign-up.  All entries could be exported by admin to a .csv file type. Laravel, Javascript, and WordPress were used in the development.',
+              demoLink: 'https://rvidaho.org/free-camping-guide/',
+          },
+          {
+              name: 'Brundage',
+              // image: require('../assets/brundage_hover.png'),
+              description: 'Updated homepage weather widget and worked to bring in a graph displaying snow data for base and summit depth',
+              demoLink: 'https://brundage.com/',
+          },
+          {
+              name: 'Kanban',
+              // image: require('../assets/kanban-hover.png'),
+              description: 'Organize work flow with custom boards, lists, tasks, and notes',
+              demoLink: "https://kanban-oconnell.herokuapp.com/",
+              gitHubLink: 'https://github.com/oconnill/vue-kanban'
+          },
+          {
+              name: 'Music Selector',
+              // image: require('../assets/music-selector-hover.png'),
+              description: 'Build a playlist of songs from the I Tunes API',
+              demoLink: 'https://vue-music-oconnell.herokuapp.com/',
+              gitHubLink: 'https://github.com/oconnill/vue-music-checkpoint'
+          },
+          {
+              name: 'Univ-Rss',
+              // image: require('../assets/univrss-hover.png'),
+              description: 'Personal dashboard for weather, rss feeds, local events, todo lists, photos, and spotify',
+              demoLink: '',
+              gitHubLink: 'https://github.com/oconnill/Univ-Rss'
+          }
+      ],
+      designOptions: [
+          {
+              name: 'Yearly Poster Project 1986 - 1994',
+              // image: require('../assets/univrss-hover.png'),
+              description: 'Personal dashboard for weather, rss feeds, local events, todo lists, photos, and spotify',
+              demoLink: '',
+              gitHubLink: 'https://github.com/oconnill/Univ-Rss'
+          }
+      ],
+      photographyOptions: [
+          {
+              name: 'Dublin Summer 1990',
+              // image: require('../assets/univrss-hover.png'),
+              description: 'Personal dashboard for weather, rss feeds, local events, todo lists, photos, and spotify',
+              demoLink: '',
+              gitHubLink: 'https://github.com/oconnill/Univ-Rss'
+          }
+      ]
     },
     mutations: {
     //   setResults(state, data) {
