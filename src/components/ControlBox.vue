e7411b31e29923b1085211622dc49dede7aa346d
oconnill <doconnelldesign@gmail.com>
2018-11-14 11:49:47 -0700

    added social icons

--- src/components/ControlBox.vue
+++ src/components/ControlBox.vue
@@ -5,6 +5,11 @@
             <div class="logo-icon" @click="menuReset()">DO</div>
         </div>
         <ControlBoxBody></ControlBoxBody>
+        <div class="control-footer">
+            <a href="https://www.linkedin.com/in/danieloconne11/"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/oconnill"><i class="fab fa-github"></i></a>
+            <a href=""><i class="far fa-envelope"></i></a>
+        </div>
     </div>
 </template>
 
