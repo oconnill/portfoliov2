bbc41cd56db6eec4bdc343d4f58cb61670e15e2b
oconnill <doconnelldesign@gmail.com>
2019-01-03 12:15:40 -0700

    Created css logo for control box

--- src/components/ControlBox.vue
+++ src/components/ControlBox.vue
@@ -1,52 +1,61 @@
 <template>
-    <div v-bind:class="{ active: isActive }" class="control-box">
-        <div class="control-header">
-            <div class="logo-font">Daniel O'Connell</div>
-            <div class="logo-icon" @click="menuReset()">DO</div>
-        </div>
-        <ControlBoxBody></ControlBoxBody>
-        <div class="control-footer">
-            <a href="https://www.linkedin.com/in/danieloconne11/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-            <a href="https://github.com/oconnill" target="_blank"><i class="fab fa-github"></i></a>
-            <a href="" target="_blank"><i class="far fa-envelope"></i></a>
-        </div>
+  <div v-bind:class="{ active: isActive }" class="control-box">
+    <div class="control-header">
+      <div class="logo-font">Daniel O'Connell</div>
+      <div class="logo-icon" @click="menuReset()">
+          <div class="do-logo">
+                <div class="do-logo-d"></div>
+                <div class="do-logo-o"></div>
+          </div>
+      </div>
     </div>
+    <ControlBoxBody></ControlBoxBody>
+    <div class="control-footer">
+      <a href="https://www.linkedin.com/in/danieloconne11/" target="_blank"
+        ><i class="fab fa-linkedin-in"></i
+      ></a>
+      <a href="https://github.com/oconnill" target="_blank"
+        ><i class="fab fa-github"></i
+      ></a>
+      <a href="" target="_blank"><i class="far fa-envelope"></i></a>
+    </div>
+  </div>
 </template>
 
 <script>
-    import ControlBoxBody from './ControlBoxBody.vue'
+  import ControlBoxBody from "./ControlBoxBody.vue";
 
-    import '../../public/controlBox.scss'
+  import "../../public/controlBox.scss";
 
-    export default {
-        name: 'app',
-        components: {
-            ControlBoxBody
-        },
-        methods: {
-            menuReset() {
-                this.$store.state.beenClicked = false;
-                this.$store.state.isActive = false;
-                this.$store.state.singlePageView = false;
-                this.$store.state.menuTitleActive = false;
-                this.menuTitleReset();
-                this.$store.dispatch('menuReset')
-            },
-            menuTitleReset() {
-                let arr = this.$store.state.menuTitles
+  export default {
+    name: "app",
+    components: {
+      ControlBoxBody
+    },
+    methods: {
+      menuReset() {
+        this.$store.state.beenClicked = false;
+        this.$store.state.isActive = false;
+        this.$store.state.singlePageView = false;
+        this.$store.state.menuTitleActive = false;
+        this.menuTitleReset();
+        this.$store.dispatch("menuReset");
+      },
+      menuTitleReset() {
+        let arr = this.$store.state.menuTitles;
 
-                for (var i = 0; i < arr.length; i++) {
-                    arr[i].show = true;
-                }
-            }
-        },
-            computed: {
-                isActive() {
-                    return this.$store.state.isActive
-                },
-                menuTitles() {
-                    return this.$store.state.menuTitles
-                }
-            }
+        for (var i = 0; i < arr.length; i++) {
+          arr[i].show = true;
         }
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$store.state.isActive;
+      },
+      menuTitles() {
+        return this.$store.state.menuTitles;
+      }
+    }
+  };
 </script>
