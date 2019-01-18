66a75d277a84bf03094ff38beada028d0e8f2579
oconnill <doconnelldesign@gmail.com>
2019-01-18 16:31:22 -0700

    created welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <div class="row">
+          <div class="col-6"></div>
+          <div class="col-6 right-center"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+  import "../../public/welcome.scss";
+
+  export default {
+    name: "app",
+    components: {},
+    methods: {
+        getImgUrl(imgUrl) {
+        if (imgUrl) {
+          return require("../assets/" + imgUrl);
+        }
+      }
+    },
+    computed: {}
+  };
+</script>
